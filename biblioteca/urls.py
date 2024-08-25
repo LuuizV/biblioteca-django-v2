444576e3a03aee05dfaa4d3d14570e7139ea53fc
LuuizV <luizjirardi@hotmail.com>
2024-08-25 18:31:23 -0300

    Initial commit

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -4,4 +4,4 @@ from core.views import LivroListCreate, LivroDetail
 urlpatterns = [
     path('livros/', LivroListCreate.as_view(), name='livros-list-create'),
     path('livros/<int:pk>/', LivroDetail.as_view(), name='livro-detail'),
-]g
+]
